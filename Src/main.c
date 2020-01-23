7f4999006526bfde4740cc461e38a8fa976b9363
ashish kumar verma <ashishverma2614@users.noreply.github.com>
2020-01-23 18:27:38 +0530

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -1,10 +1,8 @@
+/* USER CODE BEGIN Header */
 /**
   ******************************************************************************
-  * File Name          : main.c
-  * Description        : Main program body
-  * Author             : Ashish Kumar Verma 
-  * Date               : 21-01-2020
-  * Contact            : Ashish2614@gmail.com
+  * @file           : main.c
+  * @brief          : Main program body
   ******************************************************************************
   ** This notice applies to any and all portions of this file
   * that are not between comment pairs USER CODE BEGIN and
@@ -38,39 +36,88 @@
   *
   ******************************************************************************
   */
+/* USER CODE END Header */
+
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "stm32f1xx_hal.h"
 #include "spi.h"
 #include "gpio.h"
 
+/* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include <stdio.h>
 #include "SX1278.h"
-
+/*The module has to be connected with the MCU board with following pins:
+*
+*	MCU pin		SX1278 pin	Description
+*	--------------------------------------
+*	PA4		NSS		SPI chip--select
+*	PA5		SCK		SPI SCK
+*	PA6		SO 		SPI MISO
+*	PA7		SI		SPI MOSI
+*	PB0		DIO0		LoRa interrupt indicator pin (for receiver mode)
+*	PB1		RST		LoRa reset
+*	VDD		VCC		+3V3
+*	VSS		GND		GND
+*	--------------------------------------
+*/
 /* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
 /* Private variables ---------------------------------------------------------*/
-int _write(int file, char *ptr, int len) {
-	int i;
-	for (i = 0; i < len; i++) {
-		ITM_SendChar(*ptr++);
-	}
-	return len;
+
+//struct __FILE {int handle;/* Add whatever you need here */};
+
+//FILE __stdout;
+//FILE __stdin;
+
+//int fputc(int ch, FILE *f)
+//{
+  //ITM_SendChar(ch);
+  //return(ch);
+//}
+
+ int fputc(int c, FILE *stream)
+{
+  return(ITM_SendChar(c));
 }
+ 
+//int _write(int file, char *ptr, int len) 
+	//{
+		//int i;
+		//for (i = 0; i < len; i++) 
+		//{
+			//ITM_SendChar(*ptr++);
+		//}
+	//return len;
+//}
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-
 /* USER CODE BEGIN PFP */
 /* Private function prototypes -----------------------------------------------*/
 
 /* USER CODE END PFP */
 
+/* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
 SX1278_hw_t SX1278_hw;
@@ -85,14 +132,17 @@ int message;
 int message_length;
 /* USER CODE END 0 */
 
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
 int main(void)
 {
-
   /* USER CODE BEGIN 1 */
-
+		
   /* USER CODE END 1 */
 
-  /* MCU Configuration----------------------------------------------------------*/
+  /* MCU Configuration--------------------------------------------------------*/
 
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
@@ -111,17 +161,20 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_SPI1_Init();
-
   /* USER CODE BEGIN 2 */
 //checking PB2 pin is HIGH or LOW 
+printf("starting Programm \r\n"); // for testing debugging 
 master = HAL_GPIO_ReadPin(MODE_GPIO_Port, MODE_Pin);    
-	if (master == 1) {
-		printf("Mode: Master\r\n");
-		HAL_GPIO_WritePin(MODE_GPIO_Port, MODE_Pin, GPIO_PIN_RESET);
-	} else {
-		printf("Mode: Slave\r\n");
-		HAL_GPIO_WritePin(MODE_GPIO_Port, MODE_Pin, GPIO_PIN_SET);
-	}
+	if (master == 1)
+		{
+			printf("Mode: Master\r\n");
+			HAL_GPIO_WritePin(MODE_GPIO_Port, MODE_Pin, GPIO_PIN_RESET);
+		}
+	else 
+		{
+			printf("Mode: Slave\r\n");
+			HAL_GPIO_WritePin(MODE_GPIO_Port, MODE_Pin, GPIO_PIN_SET);
+		}
 
 	//initialize LoRa module
 	SX1278_hw.dio0.port = DIO0_GPIO_Port;
@@ -141,9 +194,12 @@ master = HAL_GPIO_ReadPin(MODE_GPIO_Port, MODE_Pin);
 	
 	printf("Done configuring LoRaModule\r\n");
 
-	if (master == 1) {
+	if (master == 1) 
+	{
 		ret = SX1278_LoRaEntryTx(&SX1278, 16, 2000);
-	} else {
+	} else 
+	
+	{
 		ret = SX1278_LoRaEntryRx(&SX1278, 16, 2000);
 	}
 
@@ -153,7 +209,12 @@ master = HAL_GPIO_ReadPin(MODE_GPIO_Port, MODE_Pin);
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-		if (master == 1) {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+		 
+		if (master == 1) 
+			{
 			printf("Master ...\r\n");
 			HAL_Delay(2500);
 			printf("Sending package...\r\n");
@@ -170,7 +231,8 @@ master = HAL_GPIO_ReadPin(MODE_GPIO_Port, MODE_Pin);
 			printf("Transmission: %d\r\n", ret);
 			printf("Package sent...\r\n");
 
-		} else {
+		}
+		else {
 			printf("Slave ...\r\n");
 			HAL_Delay(1000);
 			printf("Receiving package...\r\n");
@@ -184,21 +246,23 @@ master = HAL_GPIO_ReadPin(MODE_GPIO_Port, MODE_Pin);
 			printf("Package received ...\r\n");
 
 		}
-	}
+	
+  }
+	
   /* USER CODE END 3 */
-
 }
 
-/** System Clock Configuration
-*/
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
 void SystemClock_Config(void)
 {
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  RCC_OscInitTypeDef RCC_OscInitStruct;
-  RCC_ClkInitTypeDef RCC_ClkInitStruct;
-
-    /**Initializes the CPU, AHB and APB busses clocks 
-    */
+  /**Initializes the CPU, AHB and APB busses clocks 
+  */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
@@ -208,11 +272,10 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
-    _Error_Handler(__FILE__, __LINE__);
+    Error_Handler();
   }
-
-    /**Initializes the CPU, AHB and APB busses clocks 
-    */
+  /**Initializes the CPU, AHB and APB busses clocks 
+  */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
@@ -222,66 +285,44 @@ void SystemClock_Config(void)
 
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
-    _Error_Handler(__FILE__, __LINE__);
+    Error_Handler();
   }
-
-    /**Configure the Systick interrupt time 
-    */
-  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
-
-    /**Configure the Systick 
-    */
-  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
-
-  /* SysTick_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
 }
 
 /* USER CODE BEGIN 4 */
+
 
 /* USER CODE END 4 */
 
 /**
   * @brief  This function is executed in case of error occurrence.
-  * @param  None
   * @retval None
   */
-void _Error_Handler(char * file, int line)
+void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   while(1) 
   {
   }
-  /* USER CODE END Error_Handler_Debug */ 
+  /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef USE_FULL_ASSERT
-
+#ifdef  USE_FULL_ASSERT
 /**
-   * @brief Reports the name of the source file and the source line number
-   * where the assert_param error has occurred.
-   * @param file: pointer to the source file name
-   * @param line: assert_param error line source number
-   * @retval None
-   */
-void assert_failed(uint8_t* file, uint32_t line)
-{
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{ 
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
-
 }
-
-#endif
-
-/**
-  * @}
-  */ 
-
-/**
-  * @}
-*/ 
+#endif /* USE_FULL_ASSERT */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
